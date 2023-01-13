8bff08fcc6224c77724fd015be622bf13a44903c
Sakthikprakashc <sakthikprakashc@gmail.com>
2023-01-13 14:38:54 +0530

    Implemented Login Form

--- BigFileManagement/BigFileManagement/LoginForm.cs
+++ BigFileManagement/BigFileManagement/LoginForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BigFileManagement
 {
-    public partial class Form3 : Form
+    public partial class LoginForm : Form
     {
-        public Form3()
+        public LoginForm()
         {
             InitializeComponent();
         }
